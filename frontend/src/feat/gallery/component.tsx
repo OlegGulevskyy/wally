6956f67d4e6633985efdb308c934cbe16aa94982
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-12-12 23:33:46 +0100

    gallery card layout with hover menu

--- frontend/src/feat/gallery/component.tsx
+++ frontend/src/feat/gallery/component.tsx
@@ -1,23 +1,59 @@
-import { Card, Image, Grid, createStyles } from "@mantine/core";
+import { useState } from "react";
+import { Card, Image, Grid, createStyles, Button, Group } from "@mantine/core";
 import { getGalleryImages } from "./state";
 import type { Image as ImageType } from "./types";
 
-const useStyles = createStyles((theme, _, ref) => ({
-  image: {
-		transitions: "all 0.3s ease",
-		WebkitTransition: "all 0.3s ease",
-    "&:hover": {
-      WebkitFilter: "brightness(70%)",
-    },
+const useStyles = createStyles(() => ({
+  card: {
+    position: "relative",
+  },
+	cardMenu: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		height: "100%",
+		width: "100%",
+		background: "rgba(0, 0, 0, 0.5)",
+	},
+  cardButtons: {
+    position: "absolute",
+    top: "70%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
   },
 }));
 
 const ImageCard = ({ src, alt }: ImageType) => {
-	const { classes } = useStyles()
+  const { classes } = useStyles();
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <Card shadow="sm" p="xs" radius="md">
+    <Card
+      shadow="sm"
+      p="xs"
+      radius="md"
+      onMouseEnter={() => setShowMenu(() => true)}
+      onMouseLeave={() => setShowMenu(() => false)}
+      className={classes.card}
+    >
       <Card.Section>
-        <Image className={classes.image} fit="cover" src={src.original} height={160} alt={alt} radius="md" />
+        <Image
+          fit="cover"
+          src={src.original}
+          height={160}
+          alt={alt}
+          radius="md"
+        />
+        {showMenu && (
+          <div className={classes.cardMenu}>
+            <Group noWrap position="center" className={classes.cardButtons}>
+              <Button variant="gradient" color="cyan">Set as wallpaper</Button>
+							
+							{/* disabled for now, later some additional options, maybe */}
+              <Button variant="subtle" color="dark">Options</Button>
+            </Group>
+          </div>
+        )}
       </Card.Section>
     </Card>
   );
