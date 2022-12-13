72102603828b2071c18e435f4d4912863c5ad396
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-12-13 19:08:07 +0100

    refactor header icons

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,20 +1,53 @@
-import { ActionIcon, Container, Group, Header } from "@mantine/core";
-import { IconBrandGithub } from "@tabler/icons";
+import {
+  ActionIcon,
+  Container,
+  Group,
+  Header,
+  Tooltip,
+} from "@mantine/core";
+import { IconSettings } from "@tabler/icons";
 import { OpenUrlSystemBrowser } from "../../../wailsjs/go/main/App";
 import { Searchbar } from "../../feat/search";
 import { ThemeToggle } from "../ThemeToggle";
 import { HEADER_HEIGHT, ICON_SIZE } from "./const";
 
-const GithubButton = () => {
+const GithubSvg = ({ ...props }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    width="22"
+    height="22"
+    fill="currentColor"
+		{...props}
+  >
+    <path
+      fill-rule="evenodd"
+      d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+    ></path>
+  </svg>
+);
 
-	const openGhRepoInBrowser = () => {
-		OpenUrlSystemBrowser("https://github.com/OlegGulevskyy/wally")	
-	}
+const GithubButton = () => {
+  const openGhRepoInBrowser = () => {
+    OpenUrlSystemBrowser("https://github.com/OlegGulevskyy/wally");
+  };
 
   return (
-    <ActionIcon variant="outline" color="gray" onClick={openGhRepoInBrowser}>
-      <IconBrandGithub size={ICON_SIZE} />
+		<Tooltip label="Open source">
+    <ActionIcon variant="default" color="gray" onClick={openGhRepoInBrowser}>
+			<GithubSvg height={ICON_SIZE} width={ICON_SIZE} />
     </ActionIcon>
+		</Tooltip>
+  );
+};
+
+const PersonalSettingsButton = () => {
+  return (
+		<Tooltip label="Settings">
+			<ActionIcon variant="default" color="lime">
+				<IconSettings size={ICON_SIZE} />
+			</ActionIcon>
+		</Tooltip>
   );
 };
 
@@ -30,11 +63,12 @@ export const AppHeader = () => {
           {/* left side */}
           <div>Wally Logo</div>
 
-					<Searchbar />
+          <Searchbar />
 
           {/* right side */}
           <Group>
-						<GithubButton />
+            <PersonalSettingsButton />
+            <GithubButton />
             <ThemeToggle />
           </Group>
         </Group>
--- frontend/src/components/ThemeToggle/ThemeToggle.tsx
+++ frontend/src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, useMantineColorScheme } from "@mantine/core";
+import { ActionIcon, Tooltip, useMantineColorScheme } from "@mantine/core";
 import { IconSun, IconMoonStars } from "@tabler/icons";
 import { ICON_SIZE } from "../Header/const";
 
@@ -7,13 +7,15 @@ export const ThemeToggle = () => {
   const dark = colorScheme === "dark";
 
   return (
+		<Tooltip label={`Make it ${dark ? 'light' : 'dark'}`}>
     <ActionIcon
-      variant="outline"
+      variant="default"
       color={dark ? "yellow" : "blue"}
       onClick={() => toggleColorScheme()}
       title="Toggle color scheme"
     >
       {dark ? <IconSun size={ICON_SIZE} /> : <IconMoonStars size={ICON_SIZE} />}
     </ActionIcon>
+		</Tooltip>
   );
 };
